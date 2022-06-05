844afb8f3b109c7df45ebb0ff1fa88669a9a9b4b
akaAbdullahMateen <akaabdullahmateen@gmail.com>
2022-06-06 03:44:47 +0500

    Completed exercise5-6.cpp

--- chapter5/exercise5-6.cpp
+++ chapter5/exercise5-6.cpp
@@ -1,6 +1,45 @@
-#include <iostream>
+// Exercise 5.6: Rewrite your grading program to use the conditional operator in place of the if–else statement.
 
-int main(){
-    std::cout << "Hello, World!" << std::endl;
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::string;
+
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::vector;
+using std::string;
+
+int main() {
+    double raw = 0.0;
+    vector <string> ranges = {"A", "B", "C", "D", "F"};
+    string grade;
+    vector<string>::size_type index;
+
+    cout << ">> ";
+    cin >> raw;
+
+    if (raw > 4.0 || raw < 0.0) {
+        cerr << "ERROR: INVALID INPUT" << endl;
+        return -1;
+    }
+
+    unsigned int gpa = 41 - raw * 10;
+
+    grade = (gpa > 23) ? ranges.at(ranges.size() - 1) : (index = gpa / 6, ranges.at(index));
+
+    if (gpa <= 23){
+        if (gpa % 6 > 3) {
+        grade += '-';
+        } else if (gpa % 6 < 2) {
+            grade += '+';
+        }
+    }
+    
+    cout << "[GRADE] " << grade << endl;
+
     return 0;
 }
