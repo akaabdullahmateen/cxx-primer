faac23b801f4d459cb4c4741dfe01c54e25f093d
akaAbdullahMateen <akaabdullahmateen@gmail.com>
2022-06-06 23:10:27 +0500

    Completed exercise5-11

---
+++ chapter5/exercise5-11.cpp
@@ -0,0 +1,54 @@
+// Exercise 5.11: Modify our vowel-counting program
+// So that it also counts the number of blank spaces, tabs, and newlines read.
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main(){
+	bool is_last_newline = false;
+	string text;
+	vector<char> cache;
+	vector<char>::size_type line_count = 0;
+	string::size_type vowel_count = 0;
+	string::size_type space_count = 0;
+	cout << ">> ";
+	while(getline(cin, text)){
+		for(const char &c : text){
+			if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
+				cache.push_back(c);
+				++vowel_count;
+			}
+			else if(isspace(c)){
+				++space_count;
+			}
+		}
+		cout << ">> ";
+	}
+	cout << "\b\b\b   \b\b  \b \n";
+	cout << "[VOWELS DETECTED] : " << endl;
+	for(const char &c : cache){
+		if(line_count && !(line_count % 8)){
+			cout << endl;
+			is_last_newline = true;
+		}
+		else{
+			is_last_newline = false;
+		}
+		cout << c << " ";
+		++line_count;
+	}
+	if(!is_last_newline){
+		cout << endl;
+	}
+	cout << endl;
+	cout << "[VOWEL COUNT] : " << vowel_count << endl;
+	cout << "[SPACE COUNT] : " << space_count << endl;
+	return 0;
+}
