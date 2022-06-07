2b608b1820861d45a5e0c49a5c576e2283b4a165
akaAbdullahMateen <akaabdullahmateen@gmail.com>
2022-06-07 14:21:30 +0500

    Completed exercise5-22

---
+++ chapter5/exercise5-22.cpp
@@ -0,0 +1,10 @@
+// Exercise 5.22: The last example in this section that jumped back to begin could be better written using a loop
+// Rewrite the code to eliminate the goto
+
+int main(){
+	int sz;
+	do{
+		sz = 4;
+	} while(sz <= 0);
+	return 0;
+}
