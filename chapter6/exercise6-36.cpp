fe82f9bbe9e69bbcaf360c9bccb16020ab33c57b
akaAbdullahMateen <akaabdullahmateen@gmail.com>
2022-06-09 20:38:16 +0500

    Completed exercise6-36

---
+++ chapter6/exercise6-36.cpp
@@ -0,0 +1,23 @@
+// Exercise 6.36: Write the declaration for a function that returns a reference to an array of ten strings
+// Without using either a trailing return, decltype, or a type alias
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+string arr[10] = {"aeta", "beta", "ceta", "deta", "eeta", "feta", "geta", "heta", "ieta", "jeta"};
+
+string (&dummy_func())[10]{
+	return arr;
+}
+
+int main(){
+	string (&arr)[10] = dummy_func();
+	for(const string &s : arr){
+		cout << s << endl;
+	}
+	return 0;
+}
